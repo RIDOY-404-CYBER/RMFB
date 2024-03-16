db2725736fbd6ea4f605c05a4012261089e5673d
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2024-03-16 21:16:26 +0600

    Update RMFB.py

--- RMFB.py
+++ RMFB.py
@@ -1,1 +1,9 @@
-import RM
+import os,platform
+os.system("git pull")
+rmx=platform.architecture()[0]
+if rmx=='64bit':
+  import RM
+if rmx=='32bit':
+  import RM2
+else:print('\033[1;32m[•] Contract Admin For Help');exit()
+  
